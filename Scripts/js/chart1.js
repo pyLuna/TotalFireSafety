fd2a29632cfba728cc182e51b09733c57ff20825
mark christopher calixtro <mark.christopher.calixtro1@gmail.com>
2023-03-18 07:36:42 +0800

    Dashboard Dropdown done

--- Scripts/js/chart1.js
+++ Scripts/js/chart1.js
@@ -1,38 +1,19 @@
+console.log(data);
+
 const ctx = document.getElementById('barchart').getContext('2d');
+
+
+
 const barchart = new Chart(ctx, {
     type: 'bar',
     data: {
-
-        labels: ['Pipe', 'Fire Extinguisher', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: data.map(d => d.Name),
         datasets: [{
-            label: '',
-            data: [500, 420, 881, 100, 80, 700],
-
+            label: 'Quantity',
+            data: data.map(d => d.Quantity),
             backgroundColor: function (context) {
                 var value = context.dataset.data[context.dataIndex];
 
-                if (value > 500) {
-                    return '#09AF10';
-                }
-                else if (value > 100) {
-                    return '#FF932F';
-                }
-
-                else {
-                    return '#FF0000';
-                }
-            }
-            ,
-            borderWidth: 1,
-            hoverBackgroundColor: '#fff',
-            hoverBorderWidth: 1,
-            hoverBorderColor: '#16215B',
-        },
-        {
-            label: '',
-            data: [700, 20, 80, 600, 200, 200],
-            backgroundColor: function (context) {
-                var value = context.dataset.data[context.dataIndex];
                 if (value > 500) {
                     return '#09AF10';
                 }
@@ -48,30 +29,7 @@ const barchart = new Chart(ctx, {
             hoverBackgroundColor: '#fff',
             hoverBorderWidth: 1,
             hoverBorderColor: '#16215B',
-        },
-        {
-            label: '',
-            data: [700, 20, 80, 600, 200, 200],
-            backgroundColor: function (context) {
-                var value = context.dataset.data[context.dataIndex];
-                if (value > 500) {
-                    return '#09AF10';
-                }
-                else if (value > 100) {
-                    return '#FF932F';
-                }
-
-                else {
-                    return '#FF0000';
-                }
-            },
-
-            borderWidth: 1,
-            hoverBackgroundColor: '#fff',
-            hoverBorderWidth: 1,
-            hoverBorderColor: '#16215B',
-        },
-        ]
+        }]
     },
     options: {
         plugins: {
@@ -89,3 +47,57 @@ const barchart = new Chart(ctx, {
         },
     }
 });
+
+const classSelect = document.getElementById('class');
+const itemCategorySelect = document.getElementById('itemCategory');
+const itemNameInput = document.getElementById('itemName');
+
+classSelect.addEventListener('change', updateChartData);
+itemCategorySelect.addEventListener('change', function () {
+    itemNameInput.value = ''; // clear itemNameInput value
+    updateChartData();
+});
+itemNameInput.addEventListener('change', updateChartData);
+
+function updateChartData() {
+    const selectedClass = classSelect.value;
+    const selectedCategory = itemCategorySelect.value;
+    const selectedName = itemNameInput.value;
+
+    let filteredData = data;
+
+    // Filter by item class
+    if (selectedClass !== 'All') {
+        filteredData = filteredData.filter(d => d.Class === selectedClass);
+    }
+
+    // Filter by item category
+    if (selectedCategory !== 'Clear') {
+        filteredData = filteredData.filter(d => d.Category === selectedCategory);
+    }
+
+    // Filter by item name
+    if (selectedName !== '') {
+        filteredData = filteredData.filter(d => d.Name === selectedName);
+    }
+
+    // Update the chart data and labels
+    barchart.data.labels = filteredData.map(d => d.Name);
+    barchart.data.datasets[0].data = filteredData.map(d => d.Quantity);
+
+    // Update the chart colors based on the new data
+    barchart.data.datasets[0].backgroundColor = function (context) {
+        var value = context.dataset.data[context.dataIndex];
+
+        if (value > 500) {
+            return '#09AF10';
+        } else if (value > 100) {
+            return '#FF932F';
+        } else {
+            return '#FF0000';
+        }
+    };
+
+    // Update the chart
+    barchart.update();
+}

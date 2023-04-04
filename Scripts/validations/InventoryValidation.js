044d69489e2b25577e1792023a17b59b9dfc2f43
xytrx01 <clark.steven.arcilla@gmail.com>
2023-04-04 08:31:54 +0800

    validation checkpoint

--- Scripts/validations/InventoryValidation.js
+++ Scripts/validations/InventoryValidation.js
@@ -27,16 +27,16 @@ $(document).ready(function () {
             in_type: {
                 required: true
             },
-            inpSizeSel: {
+            inpsizesel: {
                 required: true
             },
-            inpSize: {
+            inpsize: {
                 required: true
             },
-            inpSizeQuant: {
+            inpsizequant: {
                 required: true
             },
-            sizeQuant: {
+            sizequant: {
                 required: true
             }
         },
@@ -55,23 +55,100 @@ $(document).ready(function () {
             in_type: {
                 required: "Type must be required"
             },
-            inpSizeSel: {
+            inpsizesel: {
                 required: "Size must be required"
             },
-            sizeSel: {
+            sizesel: {
                 required: ""
             },
-            inpSizeQuant: {
+            inpsizequant: {
                 required: "Quantity must be required"
             },
-            sizeQuant: {
+            sizequant: {
                 required: ""
             }
         }
     });
 });
 
- /*======================= END ==============================*/
+$(document).ready(function () {
+    const pop_up_form = `
+                                    <div class="fields">
+                                            <div class="input-field">
+                                                <label>item id</label>
+                                                <input type="text" id="in_code" name="in_code" placeholder="abc-def-ghi-000" readonly>
+                                            </div>
+                                        <div class="input-field">
+                                            <label>item name</label>
+                                            <input type="text" id="in_name" name="in_name" placeholder="enter item name" required>
+                                        </div>
+                                        <div class="input-field">
+                                            <label>category</label>
+                                            <input type="text" id="in_category" name="in_category" list="catlist" placeholder="enter item category" required>
+                                            <datalist id="catlist"></datalist>
+                                        </div>
+                                        <div class="input-field">
+                                            <label>class</label>
+                                            <select id="class" name="class" required>
+                                                <option value="" selected>select class</option>
+                                                <option>v1</option>
+                                            </select>
+                                        </div>
+                                        <input type="text" id="in_class" name="in_class" style="display:none;">
+                                        <div class="input-field">
+                                            <label>type</label>
+                                            <input type="text" id="in_type" list="typelist" name="in_type" placeholder="enter item type" required>
+                                            <datalist id="typelist"></datalist>
+                                        </div>
+                                        <div class="input-field">
+                                            <label>size</label>
+                                            <div class="input-field-size">
+                                                <input type="number" id="inpsizesel" name="inpsizesel" placeholder="00000" required>
+                                                <select id="sizesel" name="sizesel" required>
+                                                    <option value="" selected>unit</option>
+                                                    <option>Inch</option>
+                                                    <option>cm</option>
+                                                </select>
+                                            </div>
+                                            <label id="inpsizesel-error" class="error manual-error" for="inpsizesel"></label>
+                                            <label id="sizesel-error" class="error manual-error" for="sizesel"></label>
+                                        </div>
+                                        <input type="text" id="in_size" name="in_size" style="display:none;" required>
+                                        <div class="input-field">
+                                            <label>quantity</label>
+                                            <div class="input-field-quantity">
+                                                <input type="number" id="inpsizequant" name="inpsizequant" placeholder="00000" required>
+                                                <select id="sizequant" name="sizequant" required>
+                                                    <option value="" selected>unit</option>
+                                                    <option>pcs</option>
+                                                </select>
+                                            </div>
+                                            <label id="inpsizequant-error" class="error manual-error" for="inpsizequant"></label>
+                                            <label id="sizequant-error" class="error manual-error" for="sizequant"></label>
+                                        </div>
+                                        <input type="text" id="in_quantity" name="in_quantity" style="display:none!important;" required>
+                                        <input type="text" id="in_remarks" name="in_remarks" style="display:none;" required>
+                                        <div class="input-field">
+                                            <label>date added</label>
+                                            <input type="text" id="dateadded" name="in_dateadded" readonly>
+                                        </div>
+                                        <div class="input-field">
+                                            <div class="img-bar-grp" id="image-container">
+                                            
+                                            </div>
+                                        </div>
+                                    </div>`
+
+    $("#add_new_itm").on('click', function (e) {
+        $("#add-popup-inv").addClass("add-popup-inv-show");
+        $("#add-popup-inv")
+            .find(".inventory-form-contents")
+            .html(pop_up_form)
+            .closest("form")
+            .validate();
+    })
+});
+/*======================= END ==============================*/
 
 /*ADD QUANTITY VALIDATION SECTION*/
 
@@ -100,6 +177,81 @@ $(document).ready(function () {
             }
         }
     });
+});
+
+$(document).ready(function () {
+    const add_quan_pop_up_form = `<div class="fields">
+                                        <div class="input-field">
+                                            <label>Item ID</label>
+                                            <input type="text" id="in_code1" name="in_code" placeholder="ABCD-EFG-HIJ" required>
+                                        </div>
+                                            <div class="input-field">
+                                                <label>Item Name</label>
+                                                <input type="text" id="in_name1" name="in_name" placeholder="Enter item name" readonly>
+                                            </div>
+                                            <div class="input-field">
+                                                <label>Category</label>
+                                                <select id="selcat1" disabled>
+                                                    <option disabled selected>Select Category</option>
+                                                </select>
+                                            </div>
+                                            <div class="input-field">
+                                                <label>Class</label>
+                                                <select id="selclass" disabled>
+                                                    <option disabled selected>Select Class</option>
+                                                </select>
+                                            </div>
+                                            <div class="input-field">
+                                                <label>Type</label>
+                                                <select id="seltype" disabled>
+                                                    <option disabled selected>Select Type</option>
+                                                </select>
+                                            </div>
+                                        <div class="input-field">
+                                            <label>Size</label>
+                                            <div class="input-field-size">
+                                                <input type="number" id="inpSize1" name="inpSize1" placeholder="00000" readonly>
+                                                <select id="inpSize" disabled>
+                                                    <option disabled selected>Unit</option>
+                                                </select>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="input-field">
+                                            <label>Quantity</label>
+                                            <div class="input-field-quantity">
+                                                <input type="number" id="inpQuant1" name="inpQuant1" placeholder="00000" required>
+                                                <select id="inpQuant" name="inpQuant">
+                                                    <option value="" disabled selected>Unit</option>
+                                                </select>
+                                            </div>
+                                            <label id="inpQuant1-error" class="error manual-error" for="inpQuant1"></label>
+                                        </div>
+                                        <input type="text" id="in_category1" name="in_category" style="display:none;">
+                                        <input type="text" id="in_class1" name="in_class" style="display:none;">
+                                        <input type="text" id="in_type1" name="in_type" style="display:none;">
+                                        <input type="text" id="in_size1" name="in_size" style="display:none;">
+                                        <input type="text" id="in_quantity1" name="in_quantity" style="display:none;">
+                                        <input type="text" id="in_remarks1" name="in_remarks" style="display:none;">
+                                            <div class="input-field">
+                                                <label>Date Added</label>
+                                                <input type="text" id="inpDate" name="in_dateAdded" placeholder="00/00/00" readonly>
+                                            </div>
+                                            <div class="input-field">
+                                                <div class="img-bar-grp" id="image-container">
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>`
+
+    $("#add_quan11").on('click', function (e) {
+        $("#add-qty-popup-inv").addClass("add-qty-popup-inv-show");
+        $("#add-qty-popup-inv")
+            .find(".inventory-form-contents")
+            .html(add_quan_pop_up_form)
+            .closest("form")
+            .validate();
+    })
 });
 /*======================= END ==============================*/
 
@@ -165,78 +317,3 @@ $(document).ready(function () {
         }
     });
 });
-/*======================= END ==============================*/
-
-//(function ($) {
-//	"use strict";
-
-//	==================================================================
-//	[ Validate ]
-//	var input = $('.input-field input');
-//	var dropdown = $('.input-field select');
-//	console.log(tableInp);
-//	console.log(input);
-//	var message = '';
-
-//	$('#saveBtn').on('click', function () {
-//		var check = true;
-
-//		for (var i = 0; i < input.length; i++) {
-//			if (validate(input[i]) == false) {
-//				showValidate(input[i]);
-//				check = false;
-//			}
-//		}
-//		for (var i = 0; i < dropdown.length; i++) {
-//			if (validate(dropdown[i]) == false) {
-//				showValidate(dropdown[i]);
-//				check = false;
-//			}
-//		}
-//		return check;
-//	});
-
-//	$(input).each(function () {
-//		$(this).focus(function () {
-//			hideValidate(this);
-//		});
-//	});
-//	$(dropdown).each(function () {
-//		$(this).focus(function () {
-//			hideValidate(this);
-//		});
-//	});
-//	function validate(input) {
-//		if ($(input).val() == null || $(input).val().length == 0) {
-//			message = 'This field is required';
-//			return false;
-//		}
-//	}
-
-//	function showValidate(input) {
-//		var thisAlert = $(input).parent();
-
-//		$(thisAlert).addClass('alert-validate');
-//		if ($(input).attr('name') == 'request_emp') {
-//			$(thisAlert).addClass('employee_input');
-//		}
-//		if ($(input).attr('name') == 'select_type') {
-//			$(thisAlert).addClass('employee_dropdown');
-//		}
-//		if (message.length != 0) {
-//			$(thisAlert).attr('data-validate', message);
-//			message = '';
-//		}
-//		 Add style to the element with data-validation="required"
-//		$('[data-validation="This field is required"]').css({
-//			color: "red",
-//			fontWeight: "bold"
-//		});
-//	}
-
-//	function hideValidate(input) {
-//		var thisAlert = $(input).parent();
-
-//		$(thisAlert).removeClass('alert-validate');
-//	}
-//})(jQuery);
--- Scripts/validations/UserValidation.js
+++ Scripts/validations/UserValidation.js
@@ -1,12 +1,11 @@
-$(document).ready(function () {
-	$("#emp_contact").keypress(function (e) {
-		var length = this.value.length;
-		if (length >= 11) {
-			e.preventDefault();
-			/*alert("not allow more than 11 character");*/
-		}
-	});
 
+
+$(document).on('keypress', '#emp_contact', function (event) {
+	var length = this.value.length;
+	if (length >= 11) {
+		event.preventDefault();
+		return false;
+	}
 });
 
 $(document).on('keypress', '#emp_name', function (event) {
@@ -103,73 +102,13 @@ $(document).ready(function () {
 	$('#Credential\\.password').on('keyup', function () {
 		$(this).valid();
 	});
+
+	$("#addBtn").on('click', function (e) {
+		$("#add-popup").addClass("add-popup-show");
+		$("#add-popup")
+			.find(".form-contents")
+			.html(add_user_pop_up_form)
+			.closest("form")
+			.validate();
+	})
 });
-//(function ($) {
-//	"use strict";
-
-//	/*==================================================================
-//	[ Validate ]*/
-//	var input = $('.input-field input');
-//	var dropdown = $('.input-field select');
-//	console.log(input);
-//	var message = '';
-//	$('#saveBtn').on('click', function () {
-//		var check = true;
-
-//		for (var i = 0; i < input.length; i++) {
-//			if (validate(input[i]) == false) {
-//				showValidate(input[i]);
-//				check = false;
-//			}
-//		}
-//		for (var i = 0; i < dropdown.length; i++) {
-//			if (validate(dropdown[i]) == false) {
-//				showValidate(dropdown[i]);
-//				check = false;
-//			}
-//		}
-//		return check;
-//	});
-//	function validate(input) {
-//		if ($(input).val() == null || $(input).val().length == 0) {
-//			message = 'This field is required';
-//			return false;
-//		}
-//		else if ($(input).attr('name') == 'emp_name' && $(input).val().length < 4) {
-//			message = 'Minimum length of 4';
-//			return false;
-//        }
-//		else if ($(input).attr('name') == 'emp_contact' && $(input).val().length < 11) {
-//			message = 'Minimum length of 11';
-//			return false;
-//		}
-//		else if ($(input).attr('name') == 'emp_no' && $(input).val().length < 8) {
-//			message = 'Minimum length of 8';
-//			return false;
-//		}
-//		else if ($(input).attr('name') == 'Credential.username' && $(input).val().length < 3) {
-//			message = 'Minimum length of 3';
-//			return false;
-//		}
-//		else if ($(input).attr('name') == 'Credential.password' && $(input).val().length < 8) {
-//			message = 'Minimum length of 8';
-//			return false;
-//		}
-//	}
-
-//	function showValidate(input) {
-//		var thisAlert = $(input).parent();
-
-//		$(thisAlert).addClass('alert-validate');
-//		if (message.length != 0) {
-//			$(thisAlert).attr('data-validate', message);
-//			message = '';
-//		}
-//	}
-
-//	function hideValidate(input) {
-//		var thisAlert = $(input).parent();
-
-//		$(thisAlert).removeClass('alert-validate');
-//	}
-//})(jQuery);
